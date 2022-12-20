408e250436b4da2fe269e65824e17ce77e0040bd
Lysander <lysander6@gmail.com>
2022-12-20 23:35:53 +0100

    enable some null-ls servers

--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -6,5 +6,8 @@ end
 null_ls.setup {
   sources = {
     null_ls.builtins.code_actions.gitsigns,
+    null_ls.builtins.completion.spell, -- might get annoying
+    null_ls.builtins.diagnostics.todo_comments,
+    null_ls.builtins.formatting.jq,
   }
 }
