bae20fc4b08ee4f819ac1c0943a558fc936b51c4
Lysander <lysander6@gmail.com>
2022-12-21 14:58:46 +0100

    disable spelling completions

--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -6,7 +6,7 @@ end
 null_ls.setup {
   sources = {
     null_ls.builtins.code_actions.gitsigns,
-    null_ls.builtins.completion.spell, -- might get annoying
+    -- null_ls.builtins.completion.spell, -- might get annoying
     null_ls.builtins.diagnostics.todo_comments,
     null_ls.builtins.formatting.jq,
   }
