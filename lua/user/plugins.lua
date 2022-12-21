084f6a02308c8311d628ec9699d938e660ce0100
Lysander <lysander6@gmail.com>
2022-12-21 17:17:37 +0100

    move treesitter config to separate file and add javascript grammar

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -39,11 +39,6 @@ return require('packer').startup(function(use)
     run = function()
       local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
       ts_update()
-    end,
-    config = function()
-      require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'lua', 'markdown', 'markdown_inline' }
-      }
     end
   }
 
---
+++ lua/user/treesitter.lua
@@ -0,0 +1,13 @@
+local status_ok, treesitter = pcall(require, 'nvim-treesitter.configs')
+if not status_ok then
+  return
+end
+
+treesitter.setup {
+  ensure_installed = {
+    'lua',
+    'markdown',
+    'markdown_inline',
+    'javascript'
+  }
+}
