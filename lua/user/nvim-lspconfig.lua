a45f27eff96b3db7cbfdb0a465ba63228afa7ffe
Lysander <lysander6@gmail.com>
2022-12-25 12:35:04 +0100

    setup custom signs for lsp diagnostics

--- lua/user/nvim-lspconfig.lua
+++ lua/user/nvim-lspconfig.lua
@@ -52,3 +52,14 @@ mason_lsp.setup_handlers {
     }
   end,
 }
+
+local signs = {
+  { name = "DiagnosticSignError", text = "" },
+  { name = "DiagnosticSignWarn", text = "" },
+  { name = "DiagnosticSignHint", text = "" },
+  { name = "DiagnosticSignInfo", text = "" },
+}
+
+for _, sign in ipairs(signs) do
+  vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = '' })
+end
