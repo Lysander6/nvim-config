8ff57ab7e669cf0f2615a27d2184937a159379c4
Lysander <lysander6@gmail.com>
2023-01-11 00:03:24 +0100

    some netrw settings, packer.nvim bootstrap and kanagawa theme

--- init.lua
+++ init.lua
@@ -18,7 +18,46 @@ vim.o.spelloptions = "camel"
 vim.o.termguicolors = true
 vim.o.whichwrap = "b,s,h,l,<,>,[,]"
 
-vim.cmd.colorscheme("habamax")
-
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
+
+vim.g.netrw_liststyle = 3
+vim.g.netrw_banner = 0
+
+-- initialize packer.nvim
+local ensure_packer = function()
+  local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+
+  if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    vim.fn.system({
+      "git",
+      "clone",
+      "--depth=1",
+      "--filter=blob:none",
+      "https://github.com/wbthomason/packer.nvim",
+      install_path,
+    })
+    vim.cmd.packadd("packer.nvim")
+    return true
+  end
+
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+-- end packer.nvim initialization
+
+require("packer").startup(function(use)
+  use("wbthomason/packer.nvim")
+
+  use({
+    "rebelot/kanagawa.nvim",
+    config = function()
+      vim.cmd.colorscheme("kanagawa")
+    end,
+  })
+
+  if packer_bootstrap then
+    require("packer").sync()
+  end
+end)
