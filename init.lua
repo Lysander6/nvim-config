ff26761994332f1f2c938c5ac23d40ab0103dcef
Lysander <lysander6@gmail.com>
2023-01-11 19:09:10 +0100

    Setup gitsigns

--- init.lua
+++ init.lua
@@ -66,6 +66,49 @@ require("packer").startup(function(use)
     end,
   })
 
+
+  use({
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup({
+        padding = false,
+      })
+    end,
+  })
+
+  use({
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          map("n", "]c", function()
+            if vim.wo.diff then return "]c" end
+            vim.schedule(function() gs.next_hunk() end)
+            return "<Ignore>"
+          end, { expr = true })
+
+          map("n", "[c", function()
+            if vim.wo.diff then return "[c" end
+            vim.schedule(function() gs.prev_hunk() end)
+            return "<Ignore>"
+          end, { expr = true })
+
+          map({"n", "v"}, "<Leader>gS", ":Gitsigns stage_hunk<CR>")
+          map("n", "<leader>gD", function() gs.diffthis("~") end)
+        end,
+      })
+    end
+  })
+
   if packer_bootstrap then
     require("packer").sync()
   end
