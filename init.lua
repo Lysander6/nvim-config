703e2d74105e982975b20ee68dbee320cd7cba6a
Daniel Walasek <Daniel.Walasek@ringieraxelspringer.pl>
2023-01-12 19:29:34 +0100

    Add gitsigns and todo to null-ls sources

--- init.lua
+++ init.lua
@@ -250,10 +250,20 @@ require("packer").startup(function(use)
     config = function()
       local null_ls = require("null-ls")
 
+      local gitsigns = null_ls.builtins.code_actions.gitsigns.with({
+        config = {
+          filter_actions = function(title)
+            return title:lower():match("blame") == nil -- filter out blame actions
+          end,
+        },
+      })
+
       -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
       null_ls.setup({
         sources = {
+          gitsigns,
           null_ls.builtins.diagnostics.eslint,
+          null_ls.builtins.diagnostics.todo_comments,
         },
       })
     end,
