14c780579ca6d28d2b9b863ac514fb2110d33363
Daniel Walasek <Daniel.Walasek@ringieraxelspringer.pl>
2023-01-12 19:37:51 +0100

    Add nice diagnostic icons

--- init.lua
+++ init.lua
@@ -273,3 +273,14 @@ require("packer").startup(function(use)
     require("packer").sync()
   end
 end)
+
+local signs = {
+  { name = "DiagnosticSignError", text = "" },
+  { name = "DiagnosticSignWarn", text = "" },
+  { name = "DiagnosticSignHint", text = "" },
+  { name = "DiagnosticSignInfo", text = "" },
+}
+
+for _, sign in ipairs(signs) do
+  vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = '' })
+end
