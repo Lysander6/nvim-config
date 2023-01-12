ed72ede31d51f74850689b9aaf348e846bbc8db3
Daniel Walasek <Daniel.Walasek@ringieraxelspringer.pl>
2023-01-12 20:35:25 +0100

    Add `cmp` completion plugin

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 vim.o.breakindent = true
+vim.o.completeopt = "menu,menuone,noselect"
 vim.o.copyindent = true
 vim.o.cursorline = true
 vim.o.cursorlineopt = "number"
@@ -226,20 +227,32 @@ require("packer").startup(function(use)
         },
       })
 
+      local has_cmp_nvim_lsp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+      local capabilities
+
+      if has_cmp_nvim_lsp then
+        capabilities = cmp_nvim_lsp.default_capabilities()
+      else
+        capabilities = vim.lsp.protocol.make_client_capabilities()
+      end
+
       mason_lspconfig.setup_handlers({
         function(server_name)
-          lspconfig[server_name].setup({})
+          lspconfig[server_name].setup({
+            capabilities = capabilities,
+          })
         end,
         ["sumneko_lua"] = function()
-          lspconfig.sumneko_lua.setup {
+          lspconfig.sumneko_lua.setup({
+            capabilities = capabilities,
             settings = {
               Lua = {
                 diagnostics = {
-                  globals = {"vim"}
-                }
-              }
-            }
-          }
+                  globals = {"vim"},
+                },
+              },
+            },
+          })
         end,
       })
     end,
@@ -265,6 +278,83 @@ require("packer").startup(function(use)
           null_ls.builtins.diagnostics.eslint,
           null_ls.builtins.diagnostics.todo_comments,
         },
+      })
+    end,
+  })
+
+  use({
+    "hrsh7th/nvim-cmp",
+    requires = {
+      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/vim-vsnip",
+      "hrsh7th/cmp-vsnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local feedkey = function(key, mode)
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+      end
+
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif vim.fn["vsnip#available"](1) == 1 then
+              feedkey("<Plug>(vsnip-expand-or-jump)", "")
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function()
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+              feedkey("<Plug>(vsnip-jump-prev)", "")
+            end
+          end, { "i", "s" }),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end,
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "vsnip" },
+        }, {
+          { name = "buffer" },
+        })
+      })
+
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        })
       })
     end,
   })
