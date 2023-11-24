6093032fda16afdefbb1b1ca355d533d70669d5e
Lysander <lysander6@gmail.com>
2023-11-24 14:38:41 +0100

    Add jsx/tsx formatting

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -16,7 +16,9 @@ return {
     formatters_by_ft = {
       lua = { "stylua" },
       javascript = { "prettierd", "prettier" },
+      javascriptreact = { "prettierd", "prettier" },
       typescript = { "prettierd", "prettier" },
+      typescriptreact = { "prettierd", "prettier" },
       sql = { "prettierd", "prettier" },
     },
     format_on_save = { timeout_ms = 500, lsp_fallback = true },
