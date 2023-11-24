5bb744921a2b1b685e7ed4b1f808ee6dc5baa924
Lysander <lysander6@gmail.com>
2023-11-24 21:51:58 +0100

    Skip backwards compatibility routines of nvim-ts-context-commentstring

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -119,6 +119,9 @@ return {
       "JoosepAlviste/nvim-ts-context-commentstring",
       "nvim-treesitter/nvim-treesitter",
     },
+    init = function()
+      vim.g.skip_ts_context_commentstring_module = true
+    end,
     config = function(_, opts)
       opts.pre_hook = require(
         "ts_context_commentstring.integrations.comment_nvim"
