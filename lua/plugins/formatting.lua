69ca77aae941e1e7ab40d2abe25da22e41abf0bd
Lysander <lysander6@gmail.com>
2023-12-25 17:57:23 +0100

    Add commands to toggle autoformatting on save

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -11,6 +11,19 @@ return {
       mode = "",
       desc = "Format buffer",
     },
+    {
+      "<leader>t=",
+      function()
+        if vim.g.disable_autoformat or vim.b.disable_autoformat then
+          vim.cmd("FormatEnable")
+          print("Autoformatting enabled")
+        else
+          vim.cmd("FormatDisable")
+          print("Autoformatting disabled")
+        end
+      end,
+      desc = "Toggle autoformatting",
+    },
   },
   opts = {
     formatters_by_ft = {
@@ -22,7 +35,13 @@ return {
       rust = { "rustfmt" },
       sql = { "prettierd", "prettier" },
     },
-    format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    format_on_save = function(bufnr)
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+
+      return { timeout_ms = 500, lsp_fallback = true }
+    end,
     formatters = {
       shfmt = {
         prepend_args = { "-i", "2" },
@@ -31,5 +50,19 @@ return {
   },
   init = function()
     vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+
+    -- adapted from https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, { bang = true, desc = "Disable autoformatting on save" })
+
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, { desc = "Enable autoformatting on save" })
   end,
 }
